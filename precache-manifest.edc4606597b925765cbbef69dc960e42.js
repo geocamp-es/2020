521bb8704fc286c82f8d11474eda70616fb48597
elenatorro <elenatorro@gmail.com>
2020-01-23 23:21:30 +0100

    Use docs as output dir

---
+++ precache-manifest.edc4606597b925765cbbef69dc960e42.js
@@ -0,0 +1,170 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e885477f537297f9b62c",
+    "url": "/css/about.cf3a49e8.css"
+  },
+  {
+    "revision": "cecd25381862c6c1481e",
+    "url": "/css/app.93d565df.css"
+  },
+  {
+    "revision": "f40889caa25956ef4d27c2aadc4fbe13",
+    "url": "/fonts/Comfortaa/Comfortaa-VariableFont:wght.ttf"
+  },
+  {
+    "revision": "11bffad5e11322af2e8b54132a46330f",
+    "url": "/fonts/Comfortaa/OFL.txt"
+  },
+  {
+    "revision": "2a58abb891faa72b65af9fe7fbc85330",
+    "url": "/fonts/Comfortaa/README"
+  },
+  {
+    "revision": "266e51ad11e63d67171aeadebc6ab541",
+    "url": "/fonts/Comfortaa/static/Comfortaa-Bold.ttf"
+  },
+  {
+    "revision": "d5cc04b5886bf560c355ed8d03ee5718",
+    "url": "/fonts/Comfortaa/static/Comfortaa-Light.ttf"
+  },
+  {
+    "revision": "ed29306657df7159b65d4f15b978aa9c",
+    "url": "/fonts/Comfortaa/static/Comfortaa-Medium.ttf"
+  },
+  {
+    "revision": "ba851e1dff547d9128572cc4297eafab",
+    "url": "/fonts/Comfortaa/static/Comfortaa-Regular.ttf"
+  },
+  {
+    "revision": "3d62340d1d5ea6f97231c2fa5241bb53",
+    "url": "/fonts/Comfortaa/static/Comfortaa-SemiBold.ttf"
+  },
+  {
+    "revision": "cabc2d83f0db00f1c4d8ab4f5cd8a34d",
+    "url": "/fonts/Quicksand/OFL.txt"
+  },
+  {
+    "revision": "f9baef8ac0d836e6486419e282e42336",
+    "url": "/fonts/Quicksand/Quicksand-VariableFont:wght.ttf"
+  },
+  {
+    "revision": "46a8f645466d30063d4d0bbad726318d",
+    "url": "/fonts/Quicksand/README"
+  },
+  {
+    "revision": "410a26014ce7e25824ae18349d2c92d3",
+    "url": "/fonts/Quicksand/static/Quicksand-Bold.ttf"
+  },
+  {
+    "revision": "7dcc3e19fb99b72cf354f6caee2df104",
+    "url": "/fonts/Quicksand/static/Quicksand-Light.ttf"
+  },
+  {
+    "revision": "7e2479fe3619b4b56dbfc0094ff23a3c",
+    "url": "/fonts/Quicksand/static/Quicksand-Medium.ttf"
+  },
+  {
+    "revision": "9a891411c3856f2db214f3507349a424",
+    "url": "/fonts/Quicksand/static/Quicksand-Regular.ttf"
+  },
+  {
+    "revision": "feb6fb672a260bc45a8bfb278e233772",
+    "url": "/fonts/Quicksand/static/Quicksand-SemiBold.ttf"
+  },
+  {
+    "revision": "498b00078a8f194964d7a3b6b05dc063",
+    "url": "/fonts/Work_Sans/OFL.txt"
+  },
+  {
+    "revision": "b63c56a7f6cb2581264e4129bd30ce49",
+    "url": "/fonts/Work_Sans/WorkSans-Black.ttf"
+  },
+  {
+    "revision": "766db6981a8f279ef14dbdc1bc8b85b6",
+    "url": "/fonts/Work_Sans/WorkSans-Bold.ttf"
+  },
+  {
+    "revision": "1ed49c8ec155244026a0caf7fb577cf8",
+    "url": "/fonts/Work_Sans/WorkSans-ExtraBold.ttf"
+  },
+  {
+    "revision": "43d14eda77c451857e34f336666f34a1",
+    "url": "/fonts/Work_Sans/WorkSans-ExtraLight.ttf"
+  },
+  {
+    "revision": "121d613863a872004b04d5a1d782b3e4",
+    "url": "/fonts/Work_Sans/WorkSans-Light.ttf"
+  },
+  {
+    "revision": "eddbbc629def6aa237965521b3af404f",
+    "url": "/fonts/Work_Sans/WorkSans-Medium.ttf"
+  },
+  {
+    "revision": "740b8e9adcb998d4f65b3b18251fb5e2",
+    "url": "/fonts/Work_Sans/WorkSans-Regular.ttf"
+  },
+  {
+    "revision": "6ae86405365767c10f7e31039faab17d",
+    "url": "/fonts/Work_Sans/WorkSans-SemiBold.ttf"
+  },
+  {
+    "revision": "cc62024e6d20a2699b38f24d89eaf5c3",
+    "url": "/fonts/Work_Sans/WorkSans-Thin.ttf"
+  },
+  {
+    "revision": "0b9b370e9ece6706e08d6348d7d84f84",
+    "url": "/img/logos/logo-geocamp-amarillo.svg"
+  },
+  {
+    "revision": "7f12bd95432c7fbff975bc32b2273d3a",
+    "url": "/img/logos/logo-geocamp-azul.svg"
+  },
+  {
+    "revision": "c2f8406cf37f37295e62cc2443103f01",
+    "url": "/img/logos/logo-pulpo-azul-border-color.svg"
+  },
+  {
+    "revision": "a91cab96bb031ea6f22d2ffc6bdffe5f",
+    "url": "/img/logos/logo-pulpo-azul.svg"
+  },
+  {
+    "revision": "a7051c662ee6abf783a3357a619c1ddd",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e885477f537297f9b62c",
+    "url": "/js/about.34ce746d.js"
+  },
+  {
+    "revision": "cecd25381862c6c1481e",
+    "url": "/js/app.05b0d0f5.js"
+  },
+  {
+    "revision": "9f7d8af0c0be8a966146",
+    "url": "/js/chunk-vendors.e3515abb.js"
+  },
+  {
+    "revision": "ad766cd074bf8695c62db13a69068c64",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "857c4dcc9f0e6bb2b47d2ef5af73e235",
+    "url": "/maps/murcia/index.html"
+  },
+  {
+    "revision": "fc1559ec232bbf740c7091a4773f52f2",
+    "url": "/maps/murcia/municipios_murcia.geojson"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  },
+  {
+    "revision": "fd3261c828cdaa40d7556162016d97cc",
+    "url": "/scripts/map.js"
+  },
+  {
+    "revision": "ec27608fc3f8fc33b915fe5bb9dcfb82",
+    "url": "/scripts/murcia.js"
+  }
+]);
